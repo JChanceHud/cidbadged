16bc822997e9336379ad20f121684110e5d5c939
Chance Hudson <jchancehud@gmail.com>
2018-11-12 04:05:37 -0600

    refactor

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import dnslink from 'dnslink';
 import http from 'http';
-import url, { URLSearchParams } from 'url';
+import url from 'url';
 
 const server = http.createServer(async (req, res) => {
   try {
@@ -10,16 +10,27 @@ const server = http.createServer(async (req, res) => {
     };
     if (!params.domain) {
       res.statusCode = 400;
-      return res.end('supply a domain using ?domain=commontheory.io');
+      res.end('supply a domain using ?domain=commontheory.io');
+      return;
     }
     const cid = await dnslink.resolve(params.domain);
     const link = params.link || `https://ipfs.io/ipfs/${cid.replace('/ipfs/', '')}`;
-    const left = 'latest cid';
     const display = cid.replace('/ipfs/', '').slice(0, 6);
-    const width = '200';
     res.setHeader('Content-Type', 'image/svg+xml');
-    res.end(
-`<svg xmlns="http://www.w3.org/2000/svg" width="114" height="20">
+    res.end(getBadge(link, 'latest cid', display));
+  } catch (err) {
+    console.log(err);
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`internal error: ${err}`);
+  }
+});
+
+server.listen(3000, () => console.log('Serving badges on port 3000'));
+
+function getBadge(link: string, left: string, right: string) {
+  return `
+<svg xmlns="http://www.w3.org/2000/svg" width="114" height="20">
   <a href="${link}">
     <linearGradient id="b" x2="0" y2="100%">
       <stop offset="0" stop-color="#bbb" stop-opacity=".1"/>
@@ -36,15 +47,9 @@ const server = http.createServer(async (req, res) => {
     <g fill="#fff" text-anchor="middle" font-family="Verdana,DejaVu Sans,Geneva,sans-serif" font-size="11">
       <text x="31" y="15" fill="#010101" fill-opacity=".3">${left}</text>
       <text x="31" y="14">${left}</text>
-      <text x="87" y="15" fill="#010101" fill-opacity=".3">${display}</text>
-      <text x="87" y="14">${display}</text>
+      <text x="87" y="15" fill="#010101" fill-opacity=".3">${right}</text>
+      <text x="87" y="14">${right}</text>
     </g>
   </a>
-</svg>`);
-  } catch (err) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end(err);
-  }
-});
-
-server.listen(3000, () => console.log('Serving badges on port 3000'));
+</svg>`;
+}
